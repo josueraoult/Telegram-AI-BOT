6b840419504c6931ac7ee08169e7ff266a579780
josueraoult <103897395+josueraoult@users.noreply.github.com>
2025-04-13 12:45:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,5 @@ GEMINI_API_KEY = "AIzaSyAArErZGDDJx7DJwExgY_pPWmN7Tjai8nk"
 
 # Modèle Whisper utilisé pour convertir l'audio en texte (STT)
 # Les options valides : "tiny", "base", "small", "medium", "large"
-WHISPER_MODEL = "base"
+WHISPER_MODEL = "tiny"
 
-# Modèle Coqui TTS utilisé pour générer de la voix à partir du texte
-# Liste des modèles disponibles ici : https://github.com/coqui-ai/TTS#pretrained-models
-COQUI_MODEL = "tts_models/fr/css10/vits"
